d72419b8b6afa4f1ce2c529fbd7fbd5bf4c5bde1
Flor <florencgmz@gmail.com>
2023-01-26 23:02:40 -0300

    styling

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
         <>
-            <title>florencia</title>
+            <title>THE NATURE OF INFINITY</title>
             <meta name="" content=""/>
             <link rel="icon" href="/favicon-2.png"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import data from '../data.json';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LanguageIcon from '@mui/icons-material/Language';
 import Cursor from './components/Cursor';
 
 function LinkCard({ href, title, image }: {href: string; title: string; image?: string}){
@@ -11,7 +12,7 @@ function LinkCard({ href, title, image }: {href: string; title: string; image?: 
     //we don't use next Link bc all links are external
     //next Link is for fast client-side page transitions
     //between pages in app.
-    <a href={href} className="flex items-center w-full mb-3 p-1 rounded-md hover:scale-105 transition-all max-w-3xl">
+    <a href={href} target='_blank' className="flex items-center w-full mb-3 p-1 rounded-md hover:scale-105 transition-all max-w-3xl">
       <div className="flex text-center w-full">
         <div className="w-10 h-10">
           {image && (
@@ -23,7 +24,7 @@ function LinkCard({ href, title, image }: {href: string; title: string; image?: 
             height={40}/>
           )}
         </div>
-        <h2 className="flex justify-center items-center font-normal w-full text-gray-100 hover:text-indigo-300 -ml-10 text-4xl underline">{title}</h2>
+        <h2 className="flex justify-center items-center font-normal w-full text-indigo-900 hover:text-indigo-300 -ml-10 text-4xl underline">{title}</h2>
       </div>
     </a>
   )
@@ -38,10 +39,10 @@ export default function Home() {
       className="rounded-full"
       alt={data.name}
       src={data.avatar}
-      width={96}
+      width={150}
       height={96}/>
-    <h1 className="font-bold mt-4 mb-8 text-xl text-gray-400">{data.name}</h1>
-    <p className="font-light mb-8 text-gray-100 text-xs text-end max-w-3xl">{data.description}</p>
+    <h1 className="font-bold mt-4 mb-8 text-8xl text-gray-600">{data.name}</h1>
+    <p className="font-normal mb-8 text-gray-100 text-4xl text-center max-w-3xl">{data.description}</p>
     {data.links.map((link) => (
       <LinkCard key={link.href} {...link} />
     ))}
@@ -49,15 +50,21 @@ export default function Home() {
       {data.socials.map((social) => {
         if(social.title.toLowerCase().includes('twitter')) {
           return <a href={social.href} className="hover:scale-105">
-            <TwitterIcon className="fill-gray-500"/>
+            <TwitterIcon className="fill-gray-300"/>
           </a>
         }
         if(social.title.toLowerCase().includes('github')) {
           return <a href={social.href} className="hover:scale-105">
-            <GitHubIcon className="fill-gray-500"/>
+            <GitHubIcon className="fill-gray-300"/>
           </a>
         }
-      })}
+        if(social.title.toLowerCase().includes('website')) {
+          return <a href={social.href} className="hover:scale-105">
+            <LanguageIcon className="fill-gray-300"/>
+          </a>
+        }
+        }
+      )}
     </div>
   </div>
   </main>
